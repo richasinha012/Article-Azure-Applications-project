388912db1391f666ff9559c40605a688986a1ce6
richasinha012 <1rn21ai163.richasinha@gmail.com>
2025-11-17 16:14:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(_file_))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
